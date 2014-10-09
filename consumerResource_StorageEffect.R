348019995c58c229ff4261d91939e65af018fa7e
Andrew Tredennick <atredenn@Andrews-iMac.local>
2014-10-09 13:29:47 -0600

    updated for relative nonlinearity

--- consumerResource_StorageEffect.R
+++ consumerResource_StorageEffect.R
@@ -13,10 +13,10 @@ library(truncnorm)
 
 sigE <- c(0,0.4,1,2.5,5,7.5,10)
 rho <- c(0.5,0,-0.5)
-b <- c(0.5, 0.5)
+# b <- c(0.5, 0.5)
 c <- c(0.1, 0.1)
 w <- c(0.2, 0.2)
-m <- c(0.15, 0.15)
+m <- c(0.18, 0.18)
 d <- 0.5
 nTime <- 1000
 
@@ -25,12 +25,37 @@ nTime <- 1000
 #### New biomass assimilation function ------------------------
 ####
 
-getB <- function(sigE, rho, nTime){
-  varcov <- matrix(c(sigE, rho*sigE, rho*sigE, sigE), 2, 2)
-  e <- rmvnorm(n = nTime, mean = c(0,0), sigma = varcov)
-  b <- exp(e) / (1+exp(e))
-  return(b)
+# getB <- function(sigE, rho, nTime){
+#   varcov <- matrix(c(sigE, rho*sigE, rho*sigE, sigE), 2, 2)
+#   e <- rmvnorm(n = nTime, mean = c(0,0), sigma = varcov)
+#   b <- exp(e) / (1+exp(e))
+#   return(b)
+# }
+getB <- function(parms, R){
+#   attach(parms)
+  spp1 <- parms$a[1]*exp(-parms$b1[1]*exp(-parms$b2[1]*R))
+  spp2 <- parms$a[2]*exp(-parms$b1[2]*exp(-parms$b2[2]*R))   ## sp. 2 biomass assimilation
+#   detach(parms)
+  return(c(spp1, spp2))
 }
+
+parms <- list(
+  a = c(0.8,0.6),  # max growth rate for spp 1 and 2
+  b1 = c(1.5,0.8),  # right offset for growth rates 
+  b2 = c(0.1,0.5)  # rates at which max is approached
+)
+
+gomp <- function(parms,R){
+#   attach(parms)
+  out1 <- parms$a[1]*exp(-parms$b1[1]*exp(-parms$b2[1]*R))
+  out2 <- parms$a[2]*exp(-parms$b1[2]*exp(-parms$b2[2]*R))
+#   detach(parms)
+  return(cbind(out1,out2))
+}
+R <- 0:25
+tmp<-gomp(parms=parms,R)
+par(mfrow=c(1,1),mar=c(4,4,1,1))
+matplot(R,tmp,type="l",ylab="Resource uptake")
 
 
 ####
@@ -53,24 +78,27 @@ updateN <- function(b, c, w, s, d, lastN, lastR){
 #### Model simulation
 ####
 
-bSeries <- getB(sigE[4], rho[3], nTime)
+# bSeries <- getB(sigE[4], rho[3], nTime)
 N <- matrix(data = NA, nrow = nTime, ncol = 2)
 N[1,] <- c(1,1)
 R <- numeric(nTime)
 R[1] <- 0.1
 
 for(t in 2:nTime){
-  s <- rtruncnorm(n = 1, a = 0, b = Inf, mean = 15, sd = 5)
-  thisStep <- updateN(b=bSeries[t,], c, w, s, d, lastN=N[t-1,], lastR=R[t-1])
+#   s <- rtruncnorm(n = 1, a = 0, b = 13*2, mean = 13, sd = 5)
+  s <- rlnorm(n=1, mean=2.8, 0.13)
+  bTmp <- getB(parms, R[t-1])
+  thisStep <- updateN(b=bTmp, c, w, s, d, lastN=N[t-1,], lastR=R[t-1])
+#   thisStep <- updateN(b=bSeries[t,], c, w, s, d, lastN=N[t-1,], lastR=R[t-1])
 #   N[t,] <- rpois(2, thisStep[[1]])
   N[t,] <- thisStep[[1]]
   R[t] <- thisStep[[2]]
 }
 
-par(mfrow=c(3,1))
+par(mfrow=c(2,1))
 matplot(c(1:nTime), N, type="l", lwd=2)
 plot(c(1:nTime), R, type="l", lwd=1)
-matplot(c(1:nTime), bSeries, type="l", lwd=2)
+# matplot(c(1:nTime), bSeries, type="l", lwd=2)
 
 
 
