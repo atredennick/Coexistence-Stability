fe9c90891ce0621dfbb4149eb4944001cf3139dd
Andrew Tredennick <atredenn@gmail.com>
2014-11-03 11:48:08 -0700

    initial commit of simulation run script
    also moved files around

--- modelCode/continuousStorageEffect_CR_v4.R
+++ modelCode/continuousStorageEffect_CR_v4.R
@@ -22,50 +22,26 @@ K <- c(.1, .1)
 mN <- c(0.1, 0.1)
 a <- 0.5
 S <- 10
-Rmu <- 2 # mean resource pulse (on log scale)
-Rsd <- 1 # st dev of resource pulses (on log scale)
+sVar <- 10
 sigE <- c(1)
 rho <- c(-1)
 
 ####
 #### Model function
 ####
-
+#continuous model
 updateDNR <- function(t, DNR, parms){
   with(as.list(c(DNR, parms)), {
     dD1dt = -(mD[1]*D1)
     dD2dt = -(mD[2]*D2)
     dN1dt = N1*(r[1]*R/(K[1]+R)) - mN[1]*N1
     dN2dt = N2*(r[2]*R/(K[2]+R)) - mN[2]*N2
-    dRdt = a*(S-R) - (N1*(r[1]*R/(K[1]+R)) + (N2*(r[2]*R/(K[2]+R))))
+    dRdt = a*((S+rand(t))-R) - (N1*(r[1]*R/(K[1]+R)) + (N2*(r[2]*R/(K[2]+R))))
     list(c(dD1dt, dD2dt, dN1dt, dN2dt, dRdt))
   })
 }
 
-#function for transition time series
-getG <- function(sigE, rho, nTime){
-  varcov <- matrix(c(sigE, rho*sigE, rho*sigE, sigE), 2, 2)
-  e <- rmvnorm(n = nTime, mean = c(0,0), sigma = varcov)
-  g <- exp(e) / (1+exp(e))
-  return(g)
-}
-gVec <- getG(sigE = sigE, rho = rho, nTime = maxTime)
-gVec1 <- gVec[,1]
-gVec2 <- gVec[,2]
-# source("getSineTransitions.R")
-# gVec1 <- rep(gVecTmp[,1], length.out = maxTime)
-# gVec2 <- rep(gVecTmp[,2], length.out = maxTime)
-
-
-####
-#### Simulate model
-####
-Rvector <- rlnorm(maxTime,Rmu,Rsd)   # random pulses
-# Rvector <- rep(0,maxTime)
-eventdat <- data.frame(var=rep("R",maxTime),time = 1:maxTime,value=Rvector,method=rep("add",maxTime))
-# eventdat2 <- data.frame(var=rep("D",maxTime),time = 1:maxTime,value=Rvector,method=rep("mul",maxTime))
-# eventdat <- rbind(eventdat1, eventdat2)
-
+#discrete model
 gfun <- function(t, y, parms){
   with (as.list(y),{
     g1 <- gVec1[t]
@@ -78,6 +54,27 @@ gfun <- function(t, y, parms){
     return(c(D1, D2, N1, N2, R))
   })
 }
+
+
+####
+#### Simulate model
+####
+#function for transition time series
+getG <- function(sigE, rho, nTime){
+  varcov <- matrix(c(sigE, rho*sigE, rho*sigE, sigE), 2, 2)
+  e <- rmvnorm(n = nTime, mean = c(0,0), sigma = varcov)
+  g <- exp(e) / (1+exp(e))
+  return(g)
+}
+gVec <- getG(sigE = sigE, rho = rho, nTime = maxTime)
+gVec1 <- gVec[,1]
+gVec2 <- gVec[,2]
+
+#forcing for resource supply rate variations
+forcedat <- data.frame(Time = c(1:maxTime),
+                       Supply = rnorm(maxTime,0,sVar))
+forcedat[forcedat$Supply<(-S),2] <- -S #don't let supply go negative
+rand <- approxfun(x = forcedat[,1], y = forcedat[,2], method = "linear", rule = 2)
   
 simTime <- seq(1,maxTime,by=1)
 parms <- list(
