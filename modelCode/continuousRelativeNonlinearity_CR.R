e303bcb4dbf8b62bab292387de19a33d498965a0
--global user.email "atredenn@gmail.com <atredenn@gmail.com>
2014-11-13 18:30:01 -0700

    parameter value changes

--- modelCode/continuousRelativeNonlinearity_CR.R
+++ modelCode/continuousRelativeNonlinearity_CR.R
@@ -28,18 +28,15 @@ maxTime <- 2000 #simulation run time
 c <- c(1,1) #not used for now, could be a "cost" parameter for biomass storage
 b <- c(0.5, 0.5) #also not used, could be assimilation efficiency
 mD <- c(0.0001, 0.0001) #dormant state continuous death rate
-r <- c(2.1, 1.5) #live state intrinsic growth rates
-K2 <- c(100.8, 100.9) #live state half-saturation consant for growth curve as function of resource
-K <- c(25,15)
+r <- c(2.1, 0.15) #live state intrinsic growth rates
+K2 <- c(100, 200) #rate of approaching max growth rate
+K <- c(25,2) #offset for growth rate function
 mN <- c(0.1, 0.1) #live state continuous death rate
 a <- 0.5 #resource turnover rate
 S <- 5 #average resource supply rate
-sVar <- 5 #resource supply rate variability
+sVar <- 10 #resource supply rate variability
 sigE <- 0 #environmental cue variability
 rho <- 0 #environmental cue correlation between species
-
-
-
 
 
 ####
@@ -52,7 +49,7 @@ updateDNR <- function(t, DNR, parms){
     dD2dt = -(mD[2]*D2)
     dN1dt = N1*(r[1]*exp(-K[1]*(exp(-K2[1]*R)))) - mN[1]*N1
     dN2dt = N2*(r[2]*exp(-K[2]*(exp(-K2[2]*R)))) - mN[2]*N2
-    dRdt = a*(S-R) - (N1*(r[1]*R/(K[1]+R)) + (N2*(r[2]*R/(K[2]+R))))
+    dRdt = a*((S+rand(t))-R) - (N1*(r[1]*R/(K[1]+R)) + (N2*(r[2]*R/(K[2]+R))))
     list(c(dD1dt, dD2dt, dN1dt, dN2dt, dRdt)) #output
   })
 }
@@ -66,7 +63,7 @@ gfun <- function(t, y, parms){
     D2 <- D2 - (N2+D2)*g2 + N2
     N1 <- 0+(N1+D1)*g1
     N2 <- 0+(N2+D2)*g2
-    R <- Rnow[t]
+    R <- R
     return(c(D1, D2, N1, N2, R))
   })
 }
@@ -80,9 +77,10 @@ gVec1 <- rep(1,maxTime)
 gVec2 <- rep(1,maxTime)
 
 #forcing for resource supply rate variations
-#this just makes a random variable that can increase/decrease the supply rate by some amount
-#but it cannot decrease S below 0
-Rnow <- rlnorm(maxTime, 3, 1.1)
+forcedat <- data.frame(Time = c(1:maxTime),
+                       Supply = rnorm(maxTime,0,sVar))
+forcedat[forcedat$Supply<(-S),2] <- -S #don't let supply go negative
+rand <- approxfun(x = forcedat[,1], y = forcedat[,2], method = "linear", rule = 2)
 
 simTime <- seq(1,maxTime,by=1)
 parms <- list(
@@ -95,7 +93,7 @@ parms <- list(
   a = a,
   S = S
 )
-DNR <- c(D=c(0,0), N=c(20,20),R=20) #initial conditions
+DNR <- c(D=c(0,0), N=c(3000,3000),R=1) #initial conditions
 
 #run the model
 output = as.data.frame(ode(y = DNR, times = simTime, func = updateDNR,
@@ -127,9 +125,11 @@ matplot(R, tmp, type="l", col=c("darkorange", "purple"), lty=c(1,1),
 abline(h = mN[1], lty=2)
 matplot(simTime, output[,4:5], type="l", main="Live Biomass", lty=c(1,1), 
         col=c("darkorange", "purple"), xlab="Years (T)", ylab="Biomass (N)")
+matplot(simTime[1900:2000], output[1900:2000,4:5], type="l", main="Live Biomass", lty=c(1,1), 
+        col=c("darkorange", "purple"), xlab="Years (T)", ylab="Biomass (N)")
 
-barplot(cvs, names.arg = c("Spp 1", "Spp 2", "Community"), ylab="C.V.", col=c("darkorange", "purple", "grey"))
-box()
+# barplot(cvs, names.arg = c("Spp 1", "Spp 2", "Community"), ylab="C.V.", col=c("darkorange", "purple", "grey"))
+# box()
 
 
 
