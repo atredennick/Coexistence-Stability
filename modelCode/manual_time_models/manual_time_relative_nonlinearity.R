9b53bde88808a639dc1c1aa6ba48e80f61ff1563
Andrew Tredennick <atredenn@gmail.com>
2015-03-17 21:27:18 -0600

    Add resource fluctuating resource supply rate.
    Still need to make this a realistic time-series
    function that increase and then decreases to produce
    expected population dynamics.

--- modelCode/manual_time_models/manual_time_relative_nonlinearity.R
+++ modelCode/manual_time_models/manual_time_relative_nonlinearity.R
@@ -18,41 +18,46 @@ updateDNR <- function(state_variables, parameters){
   newD2 = D2 + (-(mD[2]*D2))
   newN1 = N1 + (N1*(r[1]*exp(-K[1]*(exp(-K2[1]*R)))) - mN[1]*N1)
   newN2 = N2 + (N2*(r[2]*exp(-K[2]*(exp(-K2[2]*R)))) - mN[2]*N2)
-  newR = R + (a*(S-R) - ((N1*(r[1]*exp(-K[1]*(exp(-K2[1]*R))))) + 
-                           (N2*(r[2]*exp(-K[2]*(exp(-K2[2]*R)))))))
+  newR = max(0, R + (a*(S-R) - ((N1*(r[1]*exp(-K[1]*(exp(-K2[1]*R))))) + 
+                           (N2*(r[2]*exp(-K[2]*(exp(-K2[2]*R))))))))
   return(c(newD1, newD2, newN1, newN2, newR))
 }
 
 # Test first model function
-# parameters: mD, r, K, mN, a, S
-parameters <- list(mD = c(0.0001, 0.0001),
-                   r = c(0.3, 0.2),
-                   K = c(10,1),
-                   K2 = c(100, 200),
-                   mN = c(0.05, 0.05),
-                   a = 0.5,
-                   S = 0)
+
+#TODO: make S a semi-normal function with increasing then decreasing values
+#      over the 120 days.
+S <- rlnorm(120,0,1)
 grow_days <- 120 #120 growing days
 
 # Initial values
-state_variables <- c(50, 50, 1, 1, 2)
+state_variables <- c(50, 50, 1, 1, 0)
 save_states <- matrix(nrow=grow_days, ncol=length(state_variables))
 save_states[1,] <- state_variables
 
 # Run through one growing season
 for(day in 2:grow_days){
+  parameters <- list(mD = c(0.0001, 0.0001),
+                     r = c(0.7, 0.3),
+                     K = c(10,5),
+                     K2 = c(1, 1.5),
+                     mN = c(0.01, 0.01),
+                     a = 0.5,
+                     S = S[day])
   new_states <- updateDNR(state_variables, parameters)
   state_variables <- new_states
   save_states[day,] <- new_states
 }
 
 # Plot this to make sure it is right
-par(mfrow=c(1,2))
+par(mfrow=c(1,3))
 matplot(c(1:grow_days), save_states[,c(3,4)], type="l",
         xlab="Day", ylab="Biomass")
+plot(c(1:grow_days), save_states[,5], type="l",
+     xlab="Day", ylab="Resource")
 
 # plot the growth rate functions for each species
-R <- seq(0,0.1,0.001)
+R <- seq(0,(4),0.01)
 getR <- function(r, R, K, K2){
   out1 <- (r[1]*exp(-K[1]*(exp(-K2[1]*R))))
   out2 <- (r[2]*exp(-K[2]*(exp(-K2[2]*R)))) 
