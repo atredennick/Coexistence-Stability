e0bf9c2ddbc1c5a4bb0f3431df54e178bfffcaf5
Andrew Tredennick <atredenn@gmail.com>
2015-03-18 08:02:40 -0600

    Made resource supply trail off at end of growing season.

--- modelCode/manual_time_models/manual_time_relative_nonlinearity.R
+++ modelCode/manual_time_models/manual_time_relative_nonlinearity.R
@@ -28,6 +28,7 @@ updateDNR <- function(state_variables, parameters){
 #TODO: make S a semi-normal function with increasing then decreasing values
 #      over the 120 days.
 S <- rlnorm(120,0,1)
+S[50:120] <- 0
 grow_days <- 120 #120 growing days
 
 # Initial values
