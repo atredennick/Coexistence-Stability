602f0337eb8157a061eddcda4c527a10aaf27f47
Andrew Tredennick <atredenn@gmail.com>
2015-03-17 20:37:16 -0600

    Added plot checking.
    Still a ways to go. Population biomass is spiking way
    too early. How can we lag that?

--- modelCode/manual_time_models/manual_time_relative_nonlinearity.R
+++ modelCode/manual_time_models/manual_time_relative_nonlinearity.R
@@ -5,11 +5,58 @@ updateDNR <- function(state_variables, parameters){
   D1 <- state_variables[1]
   D2 <- state_variables[2]
   N1 <- state_variables[3]
-  N1 <- state_variables[4]
-  newN = -(mD[1]*D1)
-  dD2dt = -(mD[2]*D2)
-  dN1dt = N1*(r[1]*exp(-K[1]*(exp(-K2[1]*R)))) - mN[1]*N1
-  dN2dt = N2*(r[2]*exp(-K[2]*(exp(-K2[2]*R)))) - mN[2]*N2
-  dRdt = a*(S-R) - ((N1*(r[1]*exp(-K[1]*(exp(-K2[1]*R))))) + (N2*(r[2]*exp(-K[2]*(exp(-K2[2]*R))))))
+  N2 <- state_variables[4]
+  R <- state_variables[5]
+  mD <- parameters$mD
+  r <- parameters$r
+  K <- parameters$K
+  K2 <- parameters$K2
+  mN <- parameters$mN
+  a <- parameters$a
+  S <- parameters$S
+  newD1 = D1 + (-(mD[1]*D1))
+  newD2 = D2 + (-(mD[2]*D2))
+  newN1 = N1 + (N1*(r[1]*exp(-K[1]*(exp(-K2[1]*R)))) - mN[1]*N1)
+  newN2 = N2 + (N2*(r[2]*exp(-K[2]*(exp(-K2[2]*R)))) - mN[2]*N2)
+  newR = R + (a*(S-R) - ((N1*(r[1]*exp(-K[1]*(exp(-K2[1]*R))))) + 
+                           (N2*(r[2]*exp(-K[2]*(exp(-K2[2]*R)))))))
+  return(c(newD1, newD2, newN1, newN2, newR))
 }
 
+# Test first model function
+# parameters: mD, r, K, mN, a, S
+parameters <- list(mD = c(0.0001, 0.0001),
+                   r = c(0.3, 0.2),
+                   K = c(10,1),
+                   K2 = c(100, 200),
+                   mN = c(0.05, 0.05),
+                   a = 0.5,
+                   S = 0)
+grow_days <- 120 #120 growing days
+
+# Initial values
+state_variables <- c(50, 50, 1, 1, 2)
+save_states <- matrix(nrow=grow_days, ncol=length(state_variables))
+save_states[1,] <- state_variables
+
+# Run through one growing season
+for(day in 2:grow_days){
+  new_states <- updateDNR(state_variables, parameters)
+  state_variables <- new_states
+  save_states[day,] <- new_states
+}
+
+# Plot this to make sure it is right
+par(mfrow=c(1,2))
+matplot(c(1:grow_days), save_states[,c(3,4)], type="l")
+
+# plot the growth rate functions for each species
+R <- seq(0,0.1,0.001)
+getR <- function(r, R, K, K2){
+  out1 <- (r[1]*exp(-K[1]*(exp(-K2[1]*R))))
+  out2 <- (r[2]*exp(-K[2]*(exp(-K2[2]*R)))) 
+  return(cbind(out1, out2))
+}
+tmp<-getR(parameters$r, R, parameters$K, parameters$K2)
+matplot(R, tmp, type="l", lty=c(1,1), 
+        ylab="Instantaneous growth rate (r)", xlab="Resource density (R)")
