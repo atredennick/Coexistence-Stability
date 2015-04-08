546e00a23e8ca749f6aeecb83d22f168cd696e01
Andrew Tredennick <atredenn@gmail.com>
2015-04-08 11:43:06 -0600

    Add allocation parameter for seed bank.
    This version is working with continuous seed bank
    allocation. But, it only works for really short seaons.
    I think we need to add in resource pulses throughout the
    season to make this work right. Then have a 120 day season
    and track end-of-season seed bank for stability.

--- modelCode/continuousRelativeNonlinearity_HillFxn.R
+++ modelCode/continuousRelativeNonlinearity_HillFxn.R
@@ -11,6 +11,9 @@
 ##                        resource pulse function.
 ##                     -- Adds 'Rstart' variable to saved output to track
 ##                        initial condition of resource for each season.
+##  Update:   4.8.2015 -- Adds allocation parameter (a) that continuously
+##                        draws some fraction of live biomass (N) and allocates
+##                        it to the dormant seed bank.
 
 ##  MODEL DESCRIPTION
 # Species "split" themselves between a dormant, low-mortaility stage (D) and 
@@ -26,10 +29,10 @@ rm(list=ls())
 #### Initial conditions, global variables, and parameters ------------------------
 ####
 temporal_autocorrelation <- F       # turn temporal autocorrelation on(T)/off(F)
-seasons <- 5                     # number of seasons to simulate
-seasons_to_exclude <- 1           # initial seasons to exclude from plots
-days_to_track <- 5                  # number of days to recover from odSolve
-DNR <- c(D=c(1,1),N=c(1,1),R=10)    # initial conditions
+seasons <- 500                    # number of seasons to simulate
+seasons_to_exclude <- 100           # initial seasons to exclude from plots
+days_to_track <- 2                 # number of days to simulate in odSolve
+DNR <- c(D=c(10,10),N=c(10,10),R=100)    # initial conditions
 Rmu <- 2                            # mean resource pulse (on log scale)
 Rsd <- 0.5                            # std dev of resource pulses (on log scale)
 sigE <- 2                           # environmental cue variability
@@ -39,7 +42,8 @@ parms <- list(
   alpha = c(10,0.5),                 # rate parameter for Hill function 
   beta = c(50,500),                  # shape parameter for Hill function
   mN = c(0.5,0.5),                  # live biomass loss (mortality) rates 
-  mD = c(0.001, 0.001)              # dormant biomass loss (mortality) rates
+  mD = c(0.001, 0.001),              # dormant biomass loss (mortality) rates
+  a = c(0.5,0.5)                    # allocation fraction of live biomass to seed bank
 )
 
 ####
@@ -54,10 +58,10 @@ library(deSolve); library(mvtnorm)
 ## Continuous model
 updateDNR <- function(t, DNR, parms){
   with(as.list(c(DNR, parms)), {
-    dD1dt = -(mD[1]*D1)
-    dD2dt = -(mD[2]*D2)
-    dN1dt = N1*(uptake_R(r[1], R, alpha[1], beta[1]) - mN[1])
-    dN2dt = N2*(uptake_R(r[2], R, alpha[2], beta[2]) - mN[2])
+    dD1dt = N1*a[1] - (mD[1]*D1)
+    dD2dt = N2*a[2] - (mD[2]*D2)
+    dN1dt = N1*(uptake_R(r[1], R, alpha[1], beta[1]) - mN[1] - a[1])
+    dN2dt = N2*(uptake_R(r[2], R, alpha[2], beta[2]) - mN[2] - a[2])
     dRdt = -1 * ((dN1dt + mN[1]*N1) + (dN2dt + mN[2]*N2))
     list(c(dD1dt, dD2dt, dN1dt, dN2dt, dRdt)) #output
   })
@@ -73,10 +77,10 @@ update_DNR <- function(t,DNR,gs){
   with (as.list(DNR),{
     g1 <- gs[1]
     g2 <- gs[2]
-    D1 <- D1 - (N1+D1)*g1 + N1
-    D2 <- D2 - (N2+D2)*g2 + N2
-    N1 <- 0+(N1+D1)*g1
-    N2 <- 0+(N2+D2)*g2
+    N1 <- D1*g1
+    N2 <- D2*g2
+    D1 <- D1*(1-g1)
+    D2 <- D2*(1-g2)
     R <- R + Rvector[t]
     return(c(D1, D2, N1, N2, R))
   })
@@ -99,7 +103,7 @@ getG <- function(sigE, rho, nTime){
 # Loop over seasons
 nms <- names(DNR)
 gVec <- getG(sigE = sigE, rho = rho, nTime = seasons)
-gVec[] <- 0.5
+gVec[] <- 1
 
 if(temporal_autocorrelation==FALSE){
   Rvector <- rlnorm(seasons,Rmu,Rsd) 
@@ -138,15 +142,15 @@ for(season_now in 1:seasons){
 #take out first couple seasons
 save_seasons <- subset(save_seasons, season>seasons_to_exclude)
 matplot(seq_along(along.with = save_seasons$time), 
-        save_seasons[,c("N1","N2")], type="l")
+        save_seasons[,c("D1","D2")], type="l")
 
 #plot just seasons
 end_of_season <- which(save_seasons$time == max(days))
 end_seasons <- save_seasons[end_of_season,]
-matplot(end_seasons$season, end_seasons[,c("N1","N2")], type="l")
-plot(N1 ~ Rstart, end_seasons)
-plot(N2 ~ Rstart, end_seasons)
-plot(N1 ~ N2, end_seasons)
+matplot(end_seasons$season, end_seasons[,c("D1","D2")], type="l")
+# plot(N1 ~ Rstart, end_seasons)
+# plot(N2 ~ Rstart, end_seasons)
+# plot(N1 ~ N2, end_seasons)
 
 #Plot the resource uptake function
 # R <- seq(0,100,1)
@@ -167,13 +171,13 @@ plot(N1 ~ N2, end_seasons)
 # }
 # plot(filter(rlnorm(100,2,1), filter=rep(1,3), circular=TRUE))
 
-R <- seq(0,100,1)
-out_r <- matrix(ncol=2, nrow=length(R))
-alpha <- c(10,0.5)
-beta <- c(50,500)
-for(i in 1:length(out_r)){
-  out_r[i,1] <- uptake_R(10, R[i], alpha[1], beta[1])
-  out_r[i,2] <- uptake_R(10, R[i], alpha[2], beta[2])
-}
-matplot(R, out_r, type="l")
+# R <- seq(0,100,1)
+# out_r <- matrix(ncol=2, nrow=length(R))
+# alpha <- c(10,0.5)
+# beta <- c(50,500)
+# for(i in 1:nrow(out_r)){
+#   out_r[i,1] <- uptake_R(10, R[i], alpha[1], beta[1])
+#   out_r[i,2] <- uptake_R(10, R[i], alpha[2], beta[2])
+# }
+# matplot(R, out_r, type="l")
 
