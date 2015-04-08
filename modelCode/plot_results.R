dc0d4cd4b85fe692fa2ab78fe28d44346380543b
Andrew Tredennick <atredenn@gmail.com>
2015-04-08 11:09:47 -0600

    Clean up files.

--- modelCode/plot_results.R
+++ modelCode/plot_results.R
@@ -72,3 +72,49 @@ ggplot(sim_summary, aes(x=rho, y=cv_n, color=sig_r, group=as.factor(sig_r)))+
   facet_wrap("sig_e")
 
 
+####
+#### RELATIVE NONLINEARITY
+####
+
+####
+####  Get results from desktop folder and combine --------------
+####
+directory <- "/Users/atredenn/Desktop/rel_nonlin/"
+files_to_open <- grep(".rds",list.files(directory))
+file_list <- list.files(directory)[files_to_open]
+file_list <- mixedsort(file_list) 
+num_files <- length(file_list)
+long_data <- data.frame(sim=NA,sig_r=NA,sd_n=NA,mu_n=NA,cv_n=NA,buffer=NA)
+Rsd_vec <- seq(0,2,by=0.15)  
+for(file in 1:num_files){
+  file_now <- readRDS(paste(directory,file_list[file],sep=""))
+  file_now$sig_r <- Rsd_vec[file]
+  long_data <- rbind(long_data, file_now)
+}
+saveRDS(long_data, "rel_nonlin_simulation_output.RDS")
+
+####
+####  Read in simulation results -------------------------------
+####
+sim_results <- readRDS("rel_nonlin_simulation_output.RDS")
+rows_to_remove <- which(is.na(sim_results$sim)==TRUE)
+sim_results <- sim_results[-rows_to_remove,]
+sim_summary <- ddply(sim_results, .(sig_r), summarise,
+                     sd_n = mean(sd_n),
+                     mu_n = mean(mu_n),
+                     cv_n = mean(cv_n),
+                     buffer = mean(buffer))
+
+
+####
+####  Plot results ---------------------------------------------
+####
+ggplot(sim_summary, aes(x=sig_r, y=buffer))+
+  geom_hline(aes(yintercept=1), linetype=2)+
+  geom_line()+
+  geom_point()+
+  ylab(expression(CV[community]/CV[resource]))+
+  xlab(expression(paste("Resource Variability (", sigma[r], "; on log scale)")))+
+  guides(color = guide_colorbar(title = expression(rho)))+
+  theme_bw()
+
