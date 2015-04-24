74af62f3da348fdc090420683456ad974f35f6f7
Andrew Tredennick <atredenn@gmail.com>
2015-04-24 15:14:17 -0600

    moves file to old versions folder

---
+++ modelCode/oldVersions/continuousRelativeNonlinearity_CR.R
@@ -0,0 +1,120 @@
+## Semi-discrete relative nonlinearity model of two species   ## 
+## coexisting on one essential resource                       ##
+
+##  Resource uptake function (Hill function)
+uptake_R <- function(r, R, alpha, beta){
+  return((r*R^alpha) / (beta^alpha + R^alpha))
+}
+
+R <- seq(0,100,1)
+out_r <- matrix(ncol=2, nrow=length(R))
+alpha <- c(10,0.5)
+beta <- c(50,500)
+for(i in 1:length(out_r)){
+  out_r[i,1] <- uptake_R(5, R[i], alpha[1], beta[1])
+  out_r[i,2] <- uptake_R(5, R[i], alpha[2], beta[2])
+}
+matplot(R, out_r, type="l")
+
+
+
+
+####
+#### 3/19/2014
+#### atredenn@gmail.com
+####
+
+##  MODEL DESCRIPTION
+# species "split" themselves between a dormant, low-mortaility stage (D) and 
+#   a higher mortality, high growth stage (N)
+# The single resource is R
+# There are two sources of variability: an environmental cue that drives the
+#   storage effect, and resource variability
+
+# clear the workspace
+rm(list=ls())
+
+####
+#### Initial conditions and global variables ------------------------
+####
+maxTime <- 200 
+burn.in <- maxTime/10
+DNR <- c(D=c(1,1),N=c(1,1),R=100)
+Rmu <- 2      #mean resource pulse (on log scale)
+Rsd <- 1.2    #std dev of resource pulses (on log scale)
+
+
+####
+#### Load relevant libraries ----------------------------------------
+####
+library(deSolve)
+library(mvtnorm)
+library(ggplot2)
+library(reshape2)
+library(gridExtra)
+
+
+####
+#### Model function -------------------------------------------------
+####
+## Continuous model
+updateDNR <- function(t, DNR, parms){
+  with(as.list(c(DNR, parms)), {
+    dD1dt = -(mD[1]*D1)
+    dD2dt = -(mD[2]*D2)
+    dN1dt = N1*(r[1]*exp(-k1[1]*exp(-k2[1]*R)) - mN[1])
+    dN2dt = N2*(r[2]*exp(-k1[2]*exp(-k2[2]*R)) - mN[2])
+    dRdt = -1* ((dN1dt + mN[1]*N1) + (dN2dt + mN[2]*N2))
+    list(c(dD1dt, dD2dt, dN1dt, dN2dt, dRdt)) #output
+  })
+}
+
+## Discrete model
+gfun <- function(t, y, parms){
+  with (as.list(y),{
+    g1 <- gVec1[t]
+    g2 <- gVec2[t]
+    D1 <- D1 - (N1+D1)*g1 + N1
+    D2 <- D2 - (N2+D2)*g2 + N2
+    N1 <- 0+(N1+D1)*g1
+    N2 <- 0+(N2+D2)*g2
+    R <- R + Rvector[t]
+    return(c(D1, D2, N1, N2, R))
+  })
+}
+
+
+#TODO: try to implement event function and event data at the same time
+
+####
+#### Simulate model -----------------------------------------------------
+####
+simTime <- seq(1,maxTime,by=1)
+parms <- list(
+  r = c(5,1),          #max growth rate for genotype A and a
+  k1 = c(20,10),       #right offset for growth rates 
+  k2 = c(0.08,0.4),    #rates at which max is approached
+  mN = c(0.5,0.5),     #live biomass loss (mortality) rates 
+  mD = c(0.001, 0.001) #dormant biomass loss (mortality) rates
+)
+
+# Set constant "germination" fraction for relative nonlinearity alone
+gVec <- matrix(ncol=2, nrow=maxTime)
+gVec[,] <- 0.5 
+gVec1 <- gVec[,1]
+gVec2 <- gVec[,2]
+
+# Set random resource fluctuations
+Rvector <- rlnorm(maxTime,Rmu,Rsd)
+
+# Run the model
+output = as.data.frame(ode(y = DNR, times = simTime, func = updateDNR, parms = parms,
+                           events = list(func = gfun, times=simTime)))
+
+####
+#### Make some plots ----------------------------------------------
+####
+par(mfrow=c(1,2))
+matplot(output[,1],output[,2:3],xlab="Time",ylab="N",type="l")
+plot(output[,1],output[,4],xlab="Time",ylab="Resource",type="l")
+
