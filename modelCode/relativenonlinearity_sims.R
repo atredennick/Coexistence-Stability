7c6123a44ea62b7f45d0069e6a46a0047803b23a
Andrew Tredennick <atredenn@gmail.com>
2016-02-24 15:55:12 -0700

    adds new plot

---
+++ modelCode/relativenonlinearity_sims.R
@@ -0,0 +1,35 @@
+##============================================================================##
+##                                                                            ##
+## A. Tredennick, P. Adler, and F. Adler                                      ##             
+## "How Fluctuation-Dependent Coexistence Mechanisms Affect the Temporal      ##
+##  Stability of Ecosystem Function"                                          ##
+##                                                                       2016 ##
+##============================================================================##
+
+## MAIN SIMULATION FILE FOR CONSUMER-RESOURCE SIMULATIONS
+
+####
+#### RELATIVE NONLINEARITY
+####
+
+rm(list=ls())                    # Erase the memory
+fxnfile <- "relative_nonlinearity_fxns.R"
+source(fxnfile)                  # Load the name function for the simulations
+require(parallel)                # Load the parallel package, to run the simulations in parallel.
+library(deSolve)
+library(mvtnorm)
+
+nbcores <- 4 # Set number of cores to match machine
+set.seed(123456789)
+
+nrho <- 11
+rholist <- rep(1, nrho)
+nsd <- 11
+rsdlist <- pretty(seq(0, 1, length.out=nsd), nsd)
+
+relnonlin_varvars <- expand.grid(rholist,rsdlist)
+names(relnonlin_varvars) <- c("rho", "Rsd")
+prm <- unique(relnonlin_varvars)
+relnon_outs <- mclapply(seq_len(nrow(prm)), function(i) do.call(simRelNonlinModel, prm[i,]), mc.cores=nbcores)
+saveRDS(relnon_outs, "../simulationResults/relative_nonlinearity_sims.RDS")
+
