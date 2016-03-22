dbe0098bcb58f3d7f195b0922cb00e65b26514c0
Andrew Tredennick <atredenn@gmail.com>
2016-03-22 11:02:04 -0600

    removes D dynamics from continuous model

--- modelCode/storage_effect.R
+++ modelCode/storage_effect.R
@@ -32,27 +32,27 @@ days_to_track <- 20              # number of days to simulate in odSolve
 DNR <- c(D=c(1,1),N=c(1,1),R=10) # initial conditions
 Rmu <- 3                         # mean resource pulse (on log scale)
 Rsd_annual <- 0                  # std dev of resource pulses (on log scale)
-sigE <- 4                        # environmental cue variance
+sigE <- 2                        # environmental cue variance
 rho <- 0                         # environmental cue correlation between species
 
 # Within-season parameters
 parms <- list(
   r = c(5,5),                    # max growth rate for each species
-  alpha = c(5,5),                # rate parameter for Hill function 
-  beta = c(20,20),               # shape parameter for Hill function
-  eps = c(0.2,0.19)               # resource-to-biomass efficiency
+  a = c(5,5),                    # rate parameter for Hill function 
+  b = c(20,20),                  # shape parameter for Hill function
+  eps = c(0.2,0.2)               # resource-to-biomass efficiency
 )
 
 # End-of-season transition parameters
-a1 <- 0.50                       # live-to-dormant biomass fraction; spp1
-a2 <- 0.49                       # live-to-dormant biomass fraction; spp2
-b1 <- 0                          # adult survivorship; spp1 (0 if annual, >0 if perennial)
-b2 <- 0                          # adult survivorship; spp2 (0 if annual, >0 if perennial)
-m1 <- 0.1                        # dormant mortality; spp1
-m2 <- 0.1                        # dormant mortality; spp2
+alpha1 <- 0.50                   # live-to-dormant biomass fraction; spp1
+alpha2 <- 0.49                   # live-to-dormant biomass fraction; spp2
+beta1 <- 0                       # adult survivorship; spp1 (0 if annual, >0 if perennial)
+beta2 <- 0                       # adult survivorship; spp2 (0 if annual, >0 if perennial)
+eta1 <- 0.1                      # dormant mortality; spp1
+eta2 <- 0.1                      # dormant mortality; spp2
 theta1 <- 0                      # resource recycling fraction; spp1
 theta2 <- 0                      # resource recycling fraction; spp2
-c <- 0                           # resource carry-over fraction
+nu <- 0                          # resource carry-over fraction
 
 ####
 #### Load relevant libraries ----------------------------------------
@@ -66,32 +66,30 @@ library('mvtnorm')
 #### Model functions -------------------------------------------------
 ####
 ## Continuous model
-updateDNR <- function(t, DNR, parms){
-  with(as.list(c(DNR, parms)), {
-    dD1dt = 0
-    dD2dt = 0
-    dN1dt = N1*eps[1]*(uptake_R(r[1], R, alpha[1], beta[1]))
-    dN2dt = N2*eps[2]*(uptake_R(r[2], R, alpha[2], beta[2]))
+updateNR <- function(t, NR, parms){
+  with(as.list(c(NR, parms)), {
+    dN1dt = N1*eps[1]*(uptake_R(r[1], R, a[1], b[1]))
+    dN2dt = N2*eps[2]*(uptake_R(r[2], R, a[2], b[2]))
     dRdt = -1 * (dN1dt/eps[1] + dN2dt/eps[2])
-    list(c(dD1dt, dD2dt, dN1dt, dN2dt, dRdt)) #output
+    list(c(dN1dt, dN2dt, dRdt)) #output
   })
 }
 
 ##  Resource uptake function (Hill function)
-uptake_R <- function(r, R, alpha, beta){
-  return((r*R^alpha) / (beta^alpha + R^alpha))
+uptake_R <- function(r, R, a, b){
+  return((r*R^a) / (b^a + R^a))
 }
 
 ## Discrete model
-update_DNR <- function(t,DNR,gs,a1,a2,m1,m2,b1,b2,theta1,theta2,c){
+update_DNR <- function(t,DNR,gammas,alpha1,alpha2,eta1,eta2,beta1,beta2,theta1,theta2,nu){
   with (as.list(DNR),{
-    g1 <- gs[1]
-    g2 <- gs[2]
-    D1 <- a1*N1 + D1*(1-g1)*(1-m1)
-    D2 <- a2*N2 + D2*(1-g2)*(1-m2)
-    N1 <- b1*(1-a1)*N1 + g1*(D1+(a1*N1))*(1-m1)
-    N2 <- b2*(1-a2)*N2 + g2*(D2+(a2*N2))*(1-m2)
-    R <- theta1*(1-a1)*N1 + theta2*(1-a2)*N2 + c*R + Rvector[t]
+    g1 <- gammas[1]
+    g2 <- gammas[2]
+    D1 <- alpha1*N1 + D1*(1-g1)*(1-eta1)
+    D2 <- alpha2*N2 + D2*(1-g2)*(1-eta2)
+    N1 <- beta1*(1-alpha1)*N1 + g1*(D1+(alpha1*N1))*(1-eta1)
+    N2 <- beta2*(1-alpha2)*N2 + g2*(D2+(alpha2*N2))*(1-eta2)
+    R <- theta1*(1-alpha1)*N1 + theta2*(1-alpha2)*N2 + nu*R + Rvector[t]
     return(c(D1, D2, N1, N2, R))
   })
 }
@@ -111,23 +109,31 @@ getG <- function(sigE, rho, nTime){
 }
 
 # Loop over seasons
-nms <- names(DNR)
+nmsDNR <- names(DNR)
+NR <- DNR[c("N1", "N2", "R")] 
+nmsNR <- names(NR)
 gVec <- getG(sigE = sigE, rho = rho, nTime = seasons)
 Rvector <- rlnorm(seasons, Rmu, Rsd_annual)
 saved_outs <- matrix(ncol=5, nrow=seasons)
 for(season_now in 1:seasons){
-  output <- ode(y = DNR, times=days,
-                func = updateDNR, parms = parms)
-  DNR <- output[nrow(output),nms]
+  output <- ode(y = NR, times=days,
+                func = updateNR, parms = parms)
+  NR <- output[nrow(output),nmsNR]
+  DNR <- c(DNR[c("D1","D2")], NR)
   saved_outs[season_now,] <- DNR
-  names(DNR) <- nms
+  names(DNR) <- nmsDNR
   DNR <- update_DNR(season_now, DNR, gVec[season_now,],
-                    a1=a1,a2=a2,m1=m1,m2=m2,b1=b1,b2=b2,
-                    theta1=theta1,theta2=theta2,c=c)
-  names(DNR) <- nms
+                    alpha1=alpha1,alpha2=alpha2,
+                    eta1=eta1,eta2=eta2,
+                    beta1=beta1,beta2=beta2,
+                    theta1=theta1,theta2=theta2,nu=nu)
+  names(DNR) <- nmsDNR
+  NR <- DNR[c("N1", "N2", "R")] 
+  names(NR) <- nmsNR
 }
-
-matplot(saved_outs[101:seasons,c(3:4)], type="l")
+matplot(saved_outs[101:seasons,c(3:4)], type="l", 
+        xlab="Season", ylab="Abundance",
+        main=expression(paste(alpha[1],"= 0.5; ", alpha[2],"=0.49; ", sigma[E],"=0.2")))
 
 
 proc.time() - ptm
