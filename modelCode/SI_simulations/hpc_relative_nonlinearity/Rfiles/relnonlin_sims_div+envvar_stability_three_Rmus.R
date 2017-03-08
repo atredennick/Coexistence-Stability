4cccada329115af8ec2df5edf3c9f55a6cf651a9
Andrew Tredennick <atredenn@gmail.com>
2017-03-08 16:37:11 -0700

    adds four mus results for relnonlin SI and updates SI text

---
+++ modelCode/SI_simulations/hpc_relative_nonlinearity/Rfiles/relnonlin_sims_div+envvar_stability_three_Rmus.R
@@ -0,0 +1,83 @@
+###########################################################################
+##  storageeffect_sims_div+envar_stability.R: runs simulations of the    ##
+##  multispecies model at different levels of resource variance and all  ##
+##  combinations of species.                                             ##
+###########################################################################
+
+####
+#### ISSUE THIE COMMAND IN SHELL BEFORE STARTING R: export OMP_NUM_THREADS=1 
+####
+
+rm(list=ls())                    # Erase the memory
+fxnfile <- "simulate_model_function_4species.R"
+source(fxnfile)                  # Load the function for the simulations
+library(deSolve)
+library(mvtnorm)
+library(Matrix)
+
+##  Get simulation id from SLURM
+args <- commandArgs(trailingOnly = F)
+myargument <- args[length(args)]
+myargument <- sub("-","",myargument)
+i <- as.numeric(myargument)
+set.seed(123456789) # Set seed to reproduce random results
+
+## Define vectors of parameters to vary -- here, initial conditions to vary richness
+DNR <- rbind(c(D=c(1,1,1,1),N=c(1,1,1,1),R=20),
+             c(D=c(1,1,1,0),N=c(1,1,1,0),R=20),
+             c(D=c(1,1,0,0),N=c(1,1,0,0),R=20),
+             c(D=c(1,0,0,0),N=c(1,0,0,0),R=20))
+
+## Define vectors of parameters to vary
+n_rsd <- 25 # Number of seasonal standard deviation levels
+rsd_vec <- pretty(seq(0.1, 1.4, length.out=n_rsd), n_rsd) # Make a pretty vector
+prm <- expand.grid(as.matrix(rsd_vec), 1:4, 1:4)
+DNR_repped <- matrix(rep(DNR,each=(nrow(prm)/nrow(DNR))),ncol=ncol(DNR))
+colnames(prm) <- c("Rsd_annual", "Rmu", "dnr_id")
+colnames(DNR_repped) <- colnames(DNR)
+prm <- cbind(prm, DNR_repped)
+prm_full <- subset(prm, select = -c(dnr_id))
+
+
+##  Define constant parameters in list
+constant_parameters <- list (
+  seasons = 5000,                  # number of seasons to simulate
+  days_to_track = 100,             # number of days to simulate in odSolve
+  # Rmu = 3,                         # mean resource pulse (on log scale)
+  # Rsd_annual = 1.2,               # std dev of resource pulses (on log scale)
+  sigE = 0,                        # environmental cue variance
+  rho = 1,                         # environmental cue correlation between species
+  alpha1 = 0.5,                    # live-to-dormant biomass fraction; spp1
+  alpha2 = 0.5,                    # live-to-dormant biomass fraction; spp2
+  alpha3 = 0.5,                    # live-to-dormant biomass fraction; spp3
+  alpha4 = 0.5,                    # live-to-dormant biomass fraction; spp4
+  eta1 = 0.1,                      # dormant mortality; spp1
+  eta2 = 0.1,                      # dormant mortality; spp2
+  eta3 = 0.1,                      # dormant mortality; spp3
+  eta4 = 0.1                       # dormant mortality; spp4
+)
+
+# Growth function parameters
+grow_parameters <- list (
+  r   = c(0.2,1.0,2.0,5.0), # max growth rate for each species
+  a   = c(2,5,10,25),       # rate parameter for Hill function 
+  b   = c(2.5,20,30,45),    # shape parameter for Hill function
+  eps = c(0.5,0.5,0.5,0.5)  # resource-to-biomass efficiency
+)
+
+
+# Make on long vector of named parameters
+constant_param_vec <- c(unlist(constant_parameters), unlist(grow_parameters))
+
+
+constant_param_matrix <- matrix(constant_param_vec, nrow = nrow(prm_full), 
+                                ncol=length(constant_param_vec), byrow = TRUE)
+colnames(constant_param_matrix) <- names(constant_param_vec)
+parameter_matrix <- cbind(constant_param_matrix, prm_full)
+
+
+##  Run all parameter combinations in paralell
+# Returns list of simulation time series with dims = c(nrow(prm), seasons, length(DNR))
+outs <- do.call(simulate_model, as.list(parameter_matrix[i,]))
+
+saveRDS(outs, paste0("relnonlin_div+envvar_cv_stable2unstable_three_Rmus_", i, ".RDS"))
---
+++ modelCode/SI_simulations/plot_relnonlin_three_rmus.R
@@ -0,0 +1,147 @@
+##  plot_relnonlin_three_rmus.R: script to collate simulation time series from
+##  model runs at three levels of mean resource, 11 levels of environmental 
+##  variance. This is stable to unstable species addition.
+##  This is for the relative nonlinearity.
+##
+##  Author:       Andrew Tredennick (atredenn@gmail.com)
+##  Date created: March 8, 2017
+
+# Clear everything
+rm(list=ls(all.names = TRUE))
+
+
+
+####
+####  PACKAGES, PATH NAMES, ETC. ----
+####
+library(tidyverse)
+library(ggthemes)
+library(dplyr)
+results_path       <- "../../simulationResults/SI_results/relnonlin_four_rmus/"
+figures_path       <- "../../manuscript/components/"
+seasons_to_exclude <- 500
+number_of_files    <- length(grep("*.RDS", list.files(results_path)))
+
+
+
+####
+####  MY PLOTTING THEME ----
+####
+my_theme <- theme_few()+
+  theme(axis.text          = element_text(size=6, color="grey35"),
+        axis.title         = element_text(size=8),
+        strip.text         = element_text(size=8, color="grey35"),
+        legend.title       = element_text(size=8),
+        legend.text        = element_text(size=6, color="grey35"),
+        legend.key.size    = unit(0.3, "cm"))
+
+
+
+####
+####  Function for calculating CV ----
+####
+get_cv <- function(x, seasons_to_exclude){
+  x <- as.data.frame(x)
+  names(x)       <- c("D1", "D2", "D3", "D4", "N1", "N2", "N3", "N4", "R")
+  livestates     <- grep("N", colnames(x))
+  tmp_totbiomass <- rowSums(x[seasons_to_exclude:nrow(x),livestates])
+  tmp_cv         <- sd(tmp_totbiomass) / mean(tmp_totbiomass)
+  tmp_mean       <- mean(tmp_totbiomass)
+  tmp_sd         <- sd(tmp_totbiomass)
+  tmp_sppavg     <- colMeans(x[seasons_to_exclude:nrow(x),livestates])
+  tmp_spprich    <- length(which(tmp_sppavg > 1))
+  
+  tmp_out <- data.frame(cv              = tmp_cv, 
+                        num_species     = tmp_spprich, 
+                        temporal_mean   = tmp_mean, 
+                        temporal_stddev = tmp_sd)
+}
+
+
+
+####
+####  RECREATE PARAMETER GRID ----
+####
+DNR <- rbind(c(D=c(1,1,1,1),N=c(1,1,1,1),R=20),
+             c(D=c(1,1,1,0),N=c(1,1,1,0),R=20),
+             c(D=c(1,1,0,0),N=c(1,1,0,0),R=20),
+             c(D=c(1,0,0,0),N=c(1,0,0,0),R=20))
+
+## Define vectors of parameters to vary
+n_rsd <- 25 # Number of seasonal standard deviation levels
+rsd_vec <- pretty(seq(0.1, 1.4, length.out=n_rsd), n_rsd) # Make a pretty vector
+prm <- expand.grid(as.matrix(rsd_vec), 1:4, 1:4)
+DNR_repped <- matrix(rep(DNR,each=(nrow(prm)/nrow(DNR))),ncol=ncol(DNR))
+colnames(prm) <- c("Rsd_annual", "Rmu", "dnr_id")
+colnames(DNR_repped) <- colnames(DNR)
+prm <- cbind(prm, DNR_repped)
+prm_full <- subset(prm, select = -c(dnr_id))
+
+
+if(dim(prm_full)[1] != number_of_files) { stop("WRONG DIMENSIONS; 
+                                               CHECK PARAMETER MATRIX") }
+
+
+
+####
+####  SUMMARIZE RESULTS ----
+####
+out_files <- as.data.frame(list.files(results_path)[grep("*.RDS", list.files(results_path))]) 
+colnames(out_files) <- "file"
+suppressWarnings(
+  out_files_sep <- out_files %>%
+    separate(file, into = c("sc1", "sc2", "sc3", "sc4", "sc5", "sc6", "sc7", "id"))
+)
+out_files <- data.frame(out_files, id = as.numeric(out_files_sep[,"id"])) %>%
+  arrange(id)
+
+  
+sims_summary <- list() # empty storage
+for(i in 1:nrow(out_files)) {
+  tmp_sim <- readRDS(paste0(results_path, out_files[i,1]))
+  tmp_cv  <- get_cv(tmp_sim, seasons_to_exclude)
+  tmp_out <- data.frame(tmp_cv,
+                        Rmu  = prm_full[i,"Rmu"],
+                        Rsd  = prm_full[i,"Rsd_annual"])
+  sims_summary <- rbind(sims_summary, tmp_out)
+}
+if(nrow(sims_summary) != number_of_files) { stop("WRONG DIMENSIONS;
+                                                 CHECK OUTPUT") }
+
+# Calculate mean CV at different realized richness
+cv_means <- sims_summary %>%
+  group_by(Rmu, Rsd, num_species) %>%
+  summarise(avg_cv = mean(cv)) %>%
+  filter(num_species>0 & avg_cv<10)
+
+cv_means$Rmu <- paste0("Rmu = ", cv_means$Rmu)
+
+# Find species first occurences for lines
+firstones <- cv_means %>%
+  group_by(Rmu, num_species) %>%
+  summarise(Rsd = min(Rsd),
+            cv = avg_cv[which.min(Rsd)])
+
+
+
+####
+####  PLOT RESULTS ----
+####
+ggplot()+
+  geom_segment(data=firstones, aes(x=Rsd, y=cv, xend=Rsd, yend=0, color=as.factor(num_species)), size=0.5, alpha=0.7)+
+  geom_line(data=cv_means, aes(x=Rsd, y=avg_cv, color=as.factor(num_species)), size=0.7, alpha=0.7)+
+  geom_point(data=cv_means, aes(x=Rsd, y=avg_cv, color=as.factor(num_species)), size=1.5, alpha=1)+
+  geom_point(data=cv_means, aes(x=Rsd, y=avg_cv, color=as.factor(num_species)), size=1.5, alpha=1, shape=1, color="grey40")+
+  scale_color_brewer(palette = "Set2", name = "Species\nRichness")+
+  xlab(expression(paste("Standard deviation of resource (",sigma[R], ")")))+
+  ylab("CV of Community Biomass")+
+  facet_grid(.~Rmu, scales = "free_y")+
+  my_theme+
+  # theme(legend.position = c(0.93, 0.155))+
+  theme(legend.title=element_text(size=6, face="bold"),
+        legend.text=element_text(size=6),
+        legend.background = element_rect(colour = "lightgrey", size=0.25),
+        legend.key = element_blank(),
+        legend.key.size = unit(0.2, "cm"))+
+  guides(colour = guide_legend(override.aes = list(size=1)))
+ggsave(paste0(figures_path, "SI_relative_nonlinearity_four_rmus.png"), width = 160, height=60, units = "mm", dpi = 600)
