7db04003263cb6820a7b11781149236415c24cbe
Andrew Tredennick <atredenn@gmail.com>
2017-03-10 11:34:38 -0700

    updates eight species SI plots

--- modelCode/SI_simulations/plot_richness_variability_8species.R
+++ modelCode/SI_simulations/plot_richness_variability_8species.R
@@ -20,37 +20,11 @@ library(viridis)
 #### INITIALIZATIONS 
 ####
 # Select path to the results and figures
-path2results <- "../simulationResults/"
-path2figs <- "../manuscript/components/"
+path2results <- "../../simulationResults/SI_results/"
+path2figs <- "../../manuscript/components/"
 
 seasons_to_exclude <- 500
 mycols <- brewer.pal(3, "Set2")
-# mycols <- viridis(2, begin=0.25, end=0.7)
-# mycols <- c("#15E7A0", "#13CFE8")
-my_theme <- theme(legend.title=element_text(size=8, face="bold"),
-                  legend.text=element_text(size=8),
-                  legend.background = element_rect(colour = "grey45", size=0.5),
-                  legend.key = element_blank(),
-                  legend.key.size = unit(0.3, "cm"),
-                  panel.grid.major = element_line(colour = "white", linetype = "dotted"),
-                  panel.grid.minor = element_line(colour = "white", linetype = "dotted"),
-                  strip.background = element_blank())
-
-my_theme <- theme_bw()+
-  theme(panel.grid.major.x = element_blank(), 
-        panel.grid.minor.x = element_blank(),
-        panel.grid.minor.y = element_blank(),
-        panel.grid.major.y = element_line(color="white"),
-        panel.background   = element_rect(fill = "#EFEFEF"),
-        axis.text          = element_text(size=10, color="grey35", family = "Arial Narrow"),
-        axis.title         = element_text(size=12, family = "Arial Narrow", face = "bold"),
-        panel.border       = element_blank(),
-        axis.line.x        = element_line(color="black"),
-        axis.line.y        = element_line(color="black"),
-        strip.background   = element_blank(),
-        strip.text         = element_text(size=8, color="grey35", family = "Arial Narrow"),
-        legend.title       = element_text(size=8, family = "Arial Narrow"),
-        legend.text        = element_text(size=6, color="grey35", family = "Arial Narrow"))
 
 my_theme <- theme_few()+
   theme(axis.text          = element_text(size=6, color="grey35"),
@@ -63,17 +37,6 @@ my_theme <- theme_few()+
 ####
 ####  SPECIES RICHNESS - ENVIRONMENTAL VARIABILITY RELATIONSHIP; STORAGE EFFECT
 ####
-### Recreate parameter grid
-# ## Define vectors of parameters to vary
-# n_sig_e <- 11 # Number of cue variance levels
-# sig_e_vec <- pretty(seq(0, 5, length.out=n_sig_e), n_sig_e) # Make a pretty vector
-# n_rho <- 11 # Number of seasonal standard deviation levels
-# rho_vec <- pretty(seq(-1, 1, length.out=n_rho), n_rho) # Make a pretty vector
-# 
-# ##  Create matrix with all possible combinations of varying parameters
-# varvars <- expand.grid(sig_e_vec, rho_vec )
-# names(varvars) <- c("sigE", "rho")
-
 ##  Read in simulation results
 rho0_storage_effect <- readRDS(paste0(path2results,"storage_effect_8species_regional.RDS"))
 n_sig_e <- 100 # Number of cue variance levels
@@ -103,16 +66,16 @@ for(i in 1:length(rho0_storage_effect)){
 avg_cv_per_rich <- ddply(save_multispp_rho0, .(spprich), summarise,
                          avg_cv = mean(cv))
 
-ggplot()+
+regional <- ggplot()+
   geom_jitter(data=save_multispp_rho0, aes(x=spprich, y=cv), shape=21, color="grey40", fill=mycols[1], size=2, width=0.05, alpha=0.5)+
-  geom_point(data = avg_cv_per_rich, aes(x=spprich, y=avg_cv), shape=19, color="grey40", size=2)+
-  geom_line(data = avg_cv_per_rich, aes(x=spprich, y=avg_cv), color="grey40")+
+  # geom_point(data = avg_cv_per_rich, aes(x=spprich, y=avg_cv), shape=19, color="grey40", size=2)+
+  # geom_line(data = avg_cv_per_rich, aes(x=spprich, y=avg_cv), color="grey40")+
+  geom_smooth(data=save_multispp_rho0, aes(x=spprich, y=cv), method="loess", se=FALSE, color=mycols[1])+
   xlab("Number of Species")+
   ylab("Variability of Total\nCommunity Biomass (CV)")+
   scale_x_continuous(breaks=c(1:8), labels = c(1:8))+
   theme_bw()+
   my_theme
-fig.width <- 133/2
 # ggsave(paste0(path2figs,"regional_diversity_stability_storage_effect_8species.png"), width = fig.width, height = 60, units = "mm", dpi = 200)
 
 
@@ -123,4 +86,32 @@ fig.width <- 133/2
 ##############                                      #################
 
 
+sim_files <- list.files(paste0(path2results,"eightspp_local/"))
+sim_files <- sim_files[grep("*.RDS", sim_files)]
+eight_spp_local <- list()
+for(i in 1:length(sim_files)){
+  tmp <- as.data.frame(readRDS(paste0(path2results,"eightspp_local/",sim_files[i])))
+  names(tmp) <- c("D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", 
+                  "N1", "N2", "N3", "N4", "N5", "N6", "N7", "N8","R")
+  livestates <- grep("N", colnames(tmp))
+  tmp_totbiomass <- rowSums(tmp[seasons_to_exclude:nrow(tmp),livestates])
+  tmp_cv <- sd(tmp_totbiomass) / mean(tmp_totbiomass)
+  tmp_sppavg <- colMeans(tmp[seasons_to_exclude:nrow(tmp),livestates])
+  tmp_spprich <- length(which(tmp_sppavg > 1))
+  tmp_out <- data.frame(cv=tmp_cv,
+                        spprich=tmp_spprich)
+  eight_spp_local <- rbind(eight_spp_local, tmp_out)
+}
 
+local <- ggplot(eight_spp_local, aes(x=spprich, y=cv))+
+  geom_jitter(shape=21, color="grey40", fill=mycols[1], size=2, width=0.05, alpha=0.5) +
+  geom_smooth(method="loess", se=FALSE, color=mycols[1])+
+  xlab("Number of Species")+
+  ylab("Variability of Total\nCommunity Biomass (CV)")+
+  scale_x_continuous(breaks=c(1:8), labels = c(1:8))+
+  theme_bw()+
+  my_theme
+
+png(filename = paste0(path2figs,"SI_storage_effect_eightspp_local_regional.png"), width = 8, height=3, units = "in", res=100)
+grid.arrange(regional,local,ncol=2)
+dev.off()
